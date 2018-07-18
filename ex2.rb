f06659fdd78e14489b0496b4033cf7ebd0f648ce
ilyana19 <aries_2152@hotmail.com>
2018-07-18 10:41:09 -0400

    created the arrays and hashes

---
+++ ex2.rb
@@ -0,0 +1,32 @@
+# Arrays
+fav_colours = ["blue", "orange", "purple"]
+age = [16, 27, 31, 35]
+coin_is_head = [true, false, true, false, true]
+artists = ["Arashi", "Kajiura Yuki", "Goose House"]
+colours = [:blue, :orange, :purple]
+
+# Hash (objects)
+dnd_conditions = {
+  poisoned: 'A poisoned creature has disadvantage on Attack rolls and Ability Checks.',
+  incapacitated: 'An incapacitated creature can’t take actions or reactions.',
+  deafened: 'A deafened creature can’t hear and automatically fails any ability check that requires hearing.'
+}
+
+movies = {
+  "Howl's Moving Castle": 2004,
+  "5 Centimeters Per Second": 2007,
+  "Your Name": 2016
+}
+
+cities = {
+  toronto: 2600000,
+  vancouver: 600000,
+  montreal: 1600000
+}
+
+friend_ages = {
+  wes: 32,
+  straife: 36,
+  emissary: 31,
+  salaries: 21
+}
