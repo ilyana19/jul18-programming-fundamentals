70cef301ed417b9ebb6e6f88a61c7d9e1a977720
ilyana19 <aries_2152@hotmail.com>
2018-07-18 13:33:18 -0400

    added ex6, creating new lists/collections

--- ex2.rb
+++ ex2.rb
@@ -96,3 +96,48 @@ age.map do |age|
 end
 
 puts "Adding new favourite colours: #{fav_colours.push("green", "yellow")}"
+
+# exercise 6
+movie_list = {
+  1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+  2009 => ["Avatar", "Star Trek", "District 9"],
+  2019 => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+}
+
+phone = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ['*', 0, '#']
+]
+
+countries = [
+  country1 = {
+    name: "Canada",
+    continent: "North America",
+    is_island: false
+  },
+  country2 = {
+    name: "Japan",
+    continent: "Asia",
+    is_island: true
+  },
+  country2 = {
+    name: "France",
+    continent: "Europe",
+    is_island: false
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
