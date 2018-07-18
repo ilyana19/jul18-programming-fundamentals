341f6814a7ced5e59513c167a3054072d04f6007
ilyana19 <aries_2152@hotmail.com>
2018-07-18 12:52:15 -0400

    added ex4

--- ex2.rb
+++ ex2.rb
@@ -38,7 +38,7 @@ puts "Ages in order: #{age.sort}"
 puts "New family member: #{age.push(0).sort}"
 
 # exercise 2
-puts "Last favourite colour: #{fav_colours.last}"
+puts "\nLast favourite colour: #{fav_colours.last}"
 puts "Add North York to Cities: #{cities[:north_york] = 636000}"
 puts "Coin Flip reversed #{flip_coins = flip_coins.reverse}"
 puts "The City of #{cities.keys[0].to_s.capitalize} has a population of #{cities.values[0].to_i}."
@@ -47,17 +47,28 @@ artists.each do |artist|
 end
 
 # exercise 3
-puts "Top two artists: #{artists[0..1]}"
+puts "\nTop two artists: #{artists[0..1]}"
 movies.each do |key, value|
   puts "#{key} came out in #{value}."
 end
 puts "Family ages: #{age.sort.reverse}"
+# not optimal
 movies[:"Beauty and the Beast"] = [1991, 2017]
 movies[:"Beauty and the Beast"].each do |value|
   puts "Beauty and the Beast came out in #{value}."
 end
 
+# exercise 4
+puts "\n"
+age.each do |num|
+  if num < 30
+    puts "Ages in my family who are younger than 30: #{num}."
+  end
+end
 
-
-
+puts "The oldest one is #{age.max}."
+puts "Heads came up #{flip_coins.count("heads")} times."
+puts "#{artists.pop} is no longer a favourite of mine."
+puts "My favourite artists: #{artists}."
+puts "The City of #{cities.keys[0].to_s.capitalize} now has a population of #{cities.values[0] = 200000}."
 
